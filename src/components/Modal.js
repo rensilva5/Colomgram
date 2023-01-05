520afd3164de772625dee332ed931a9747881627
rensilva5 <renzoasilva@gmail.com>
2023-01-05 15:56:43 -0500

    created a modal

---
+++ src/components/Modal.js
@@ -0,0 +1,9 @@
+const Modal = ({ selectedImg }) => {
+    return (
+        <div className="backdrop">
+            <img src={selectedImg} alt="enlarged pic" />
+        </div>
+    );
+}
+ 
+export default Modal;
